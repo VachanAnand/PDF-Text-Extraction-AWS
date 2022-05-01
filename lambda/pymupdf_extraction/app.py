acc395c02eb1cd24be25cc84938d59e3833c7c24
Vachan Anand <vachan.anand@senexenergy.com.au>
2022-05-01 16:35:24 +1000

    Added Read PDF from S3 functionality

--- lambda/pymupdf_extraction/app.py
+++ lambda/pymupdf_extraction/app.py
@@ -1,5 +1,20 @@
+import json
+import boto3
+import csv
+import fitz
+
 def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    return { 
-        'message' : message
-    }
+    print(event)
+    s3_client = boto3.client('s3')
+    for event_record in event['Records']:
+        body = json.loads(event_record['body'])
+        for record in json.loads(body['Message'])['Records']:
+            bucket = record['s3']['bucket']['name']
+            key = record['s3']['object']['key']
+            response = s3_client.get_object(Bucket = bucket, Key = key)
+            pdf_object = response['Body']
+            doc = fitz.open(pdf_object)
+            print('Doc :',doc)
+
+
+
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -18,6 +18,5 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   queue {
     queue_arn     = aws_sqs_queue.sqs_landing.arn
     events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
   }
 }
